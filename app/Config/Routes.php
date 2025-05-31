c1b4c25c5b196087157ed44149eceb4d2784ba23
Jeruk Peras <prakosoadimas032@gmail.com>
2025-05-31 17:48:15 +0700

    index karyawan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,6 @@ $routes->get('/', 'Home::index');
 $routes->get("/home", "PagesController::index");
 $routes->get("/about", "PagesController::about");
 $routes->get("/contac", "PagesController::contac");
+
+// karyawan
+$routes->get('/karyawan', 'KaryawanController::index');
